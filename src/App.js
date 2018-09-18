18ffb0d80cb67df7a8a296a7d3a890a15a21a4f2
Marcelo Junior <marceloadsj1@gmail.com>
2018-09-17 21:02:05 -0300

    Removing unused .css and .svg files from app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./store";
-import logo from "./logo.svg";
-import "./App.css";
 
 class App extends Component {
   render() {
@@ -12,7 +10,6 @@ class App extends Component {
         <ConnectedRouter history={history}>
           <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
               <h1 className="App-title">Welcome to React</h1>
             </header>
             <p className="App-intro">
