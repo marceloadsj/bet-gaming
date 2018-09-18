84d38c74438f837789939d25ab35e7002f18c9d0
Marcelo Junior <marceloadsj1@gmail.com>
2018-09-17 23:41:07 -0300

    Importing user reducer to combine reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
+import userReducer from "./domains/user/reducer";
 
 export default combineReducers({
+  user: userReducer,
   router: routerReducer
 });
