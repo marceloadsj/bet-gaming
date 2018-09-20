1b9b0739dd54164bf6b7487682fba8491cc1f759
Marcelo Junior <marceloadsj1@gmail.com>
2018-09-19 21:50:59 -0300

    Lot of changes in HomePage, entry of a lot of comps, navigation, etc

--- src/domains/index/HomePage.js
+++ src/domains/index/HomePage.js
@@ -1,29 +1,79 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import UserLogin from "domains/user/UserLogin";
+import { Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import UserCard from "domains/user/UserCard";
+import CountriesCard from "domains/country/CountriesCard";
+import Icon from "domains/general/Icon";
+import "./HomePage.css";
 
 class HomePage extends Component {
   static propTypes = {
-    user: PropTypes.object.isRequired
+    user: PropTypes.object.isRequired,
+    pathname: PropTypes.string.isRequired
   };
 
   render() {
-    if (!this.props.user.name) return <UserLogin />;
+    if (!this.props.user.name || !this.props.user.country) {
+      return <Redirect to="/login" />;
+    }
 
-    return <div />;
+    return (
+      <Row>
+        <Col xs="12" lg="8" className="mb-3 mb-lg-0">
+          <Navbar color="dark" dark expand="xs" className="mb-3">
+            <NavbarBrand tag={Link} to="/">
+              {process.env.REACT_APP_NAME}
+            </NavbarBrand>
+
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/" active={this.props.pathname === "/"}>
+                  <Icon icon="globe-americas" /> Countries
+                </NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink
+                  tag={Link}
+                  to="/slot-machine"
+                  active={this.props.pathname === "/slot-machine"}
+                >
+                  <Icon icon="play" /> Slot Machine
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
+
+          <Switch>
+            <Route exact path="/" component={CountriesCard} />
+          </Switch>
+        </Col>
+
+        <Col xs="12" lg="4" className="index-home-page-sticky">
+          <UserCard className="sticky-top pt-3" />
+        </Col>
+      </Row>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
+    pathname: state.router.location.pathname,
     user: state.user
   };
 };
 
-// const mapDispatchToProps = dispatch => bindActionCreators(dispatch, {});
-
-export default connect(
-  mapStateToProps
-  // mapDispatchToProps
-)(HomePage);
+export default connect(mapStateToProps)(HomePage);
