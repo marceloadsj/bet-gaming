8485139e1e24f05d0d2597ce3cc83721e03f091e
Marcelo Junior <marceloadsj1@gmail.com>
2018-09-19 21:52:03 -0300

    Added icon and lot of constants to use in slot machine component and user

---
+++ src/domains/game/constants.js
@@ -0,0 +1,47 @@
+export const CHERRY = "CHERRY";
+export const LEMON = "LEMON";
+export const APPLE = "APPLE";
+export const BANANA = "BANANA";
+
+export const SPIN_SEED = 3;
+
+export const THREE_CHERRIES_PRIZE = 50;
+export const TWO_CHERRIES_PRIZE = 40;
+export const THREE_APPLES_PRIZE = 20;
+export const TWO_APPLES_PRIZE = 10;
+export const THREE_BANANAS_PRIZE = 15;
+export const TWO_BANANAS_PRIZE = 5;
+export const THREE_LEMONS_PRIZE = 3;
+
+export const FIRST_REEL_DATA = [
+  CHERRY,
+  LEMON,
+  APPLE,
+  LEMON,
+  BANANA,
+  BANANA,
+  LEMON,
+  LEMON
+];
+
+export const SECOND_REEL_DATA = [
+  LEMON,
+  APPLE,
+  LEMON,
+  LEMON,
+  CHERRY,
+  APPLE,
+  BANANA,
+  LEMON
+];
+
+export const THIRD_REEL_DATA = [
+  LEMON,
+  APPLE,
+  LEMON,
+  APPLE,
+  CHERRY,
+  LEMON,
+  BANANA,
+  LEMON
+];
---
+++ src/domains/user/constants.js
@@ -0,0 +1,2 @@
+export const INITIAL_USER_COINS = 20;
+export const SPIN_COIN_PRICE = 1;
