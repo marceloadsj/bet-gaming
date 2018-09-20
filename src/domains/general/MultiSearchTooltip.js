7f78464681f7e7d37aa4c6b242f0e08f8c9cf169
Marcelo Junior <marceloadsj1@gmail.com>
2018-09-19 21:43:25 -0300

    Uniquifying id of multisearch

--- src/domains/general/MultiSearchTooltip.js
+++ src/domains/general/MultiSearchTooltip.js
@@ -5,8 +5,8 @@ import Icon from "domains/general/Icon";
 export default () => {
   return (
     <Fragment>
-      <Icon icon="info-circle" id="countryCountriesTableInfo" />
-      <UncontrolledTooltip target="countryCountriesTableInfo">
+      <Icon icon="info-circle" id="multiSearchInfo" />
+      <UncontrolledTooltip target="multiSearchInfo">
         You can use the ';' character to do multiple searchs at once!
       </UncontrolledTooltip>
     </Fragment>
